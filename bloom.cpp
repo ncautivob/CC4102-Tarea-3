1f527b552be34fc44177d5a6f1560c6c7f9644ec
Valentina Alarcón <valentina.alarcon.y@ug.uchile.cl>
2024-07-18 11:31:38 -0400

    Add libraries for hash functions and csv reading.

--- bloom.cpp
+++ bloom.cpp
@@ -1,45 +1,71 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <cstdint>
+#include "MurmurHash3.h"
 using namespace std;
+#include "rapidcsv.h"
 
-// TODO: this doesn't work
-int stringToAscii(string S) {
-  int result = 0;
-  for (int i = 0; i < S.length(); i++) {
-    result += static_cast<int>(S[i]);
-  }
-  return result;
+// Applies the murmur hash to the word using a specific seed.
+uint32_t murmur_hash(const string& word, uint32_t seed) {
+    uint32_t hash;
+    MurmurHash3_x86_32(word.c_str(), word.size(), seed, &hash);
+    return hash;
+}
+
+// Function that creates hash functions
+function<uint32_t(const string&)> create_murmur_hash_function(uint32_t seed) {
+    return bind(murmur_hash, placeholders::_1, seed);
 }
 
 int main() { 
   int m, k;
   cin >> m >> k;
 
-  int search_size = pow(2, 10);
+  int search_size = pow(2, 10); // = |N|
 
   // [unused position, 1, ..., m]
   vector <int> M(m+1, 0);
-  // Hashing functions
-  vector <function<int(string)>> h(k);
-  vector <int> p(k);
-  // Babies' names
-  vector <string> names;
+  // Hashing functions. We want to create k different hashing functions.
+  vector <function<uint32_t(string&)>> h(k);
 
-  // TODO: library to decompose an Excel file. Idea: write in a txt file and read using if/ofstream.
-  // TODO: library to generate distinct prime numbers, in order to create the hashing functions
-
-  // Hashing functions creation
-  for (int i = 0; i < k; i++) {
-    // TODO: document why we transform a string to an int using stringToAscii(...).
-    // TODO: implement string hashing https://cp-algorithms.com/string/string-hashing.html
-    auto func = [m, i, &p](string S) { return ((stringToAscii(S) % p[i]) % m) + 1; }; 
-    h[i] = func;
+  // Create k different hash functions
+  for (int i=0; i<k; ++i) {
+      h[i] = create_murmur_hash_function(i + 1); // Seed i + 1 for uniqueness
   }
 
+  // Let's import the strings from the two csv files.
+  rapidcsv::Document doc("csv/Popular-Baby-Names-Final.csv");
+
+  // all of the names:
+  vector<string> names = doc.GetColumn<string>("Name");
+
+  rapidcsv::Document doc2("csv/Film-Names.csv");
+
+  // all of the film names:
+  vector<string> film_names = doc2.GetColumn<string>("0");
+
+  for(int j = 0; j<film_names.size(); j++){
+    cout << film_names[j] << endl;
+  }
+
+
+  // Experimentation:
+  // We should apply the bloom filter to all baby names
+  // So we have the final 'M' value.
+  // With this M value, we can then do the experimentation.
+  // For every N value, we will do this:
+  // N/
+
+  // IDEA: We should load all baby names and film names at first,
+  // that way we don't have to load them every time and we just select a random few of them
+  // for every iteration.
+
   // Insertion using Bloom's filter
-  for (string S: dict) {
-    for (int i = 0; i < k; i++) {
-      int j = h[i](S);
-      M[j] = 1;
+  for (string &S: names) { // this shouldn't say 'names'
+    for (int i=0; i<k; i++) {
+      uint32_t j = (h[i](S) % m) + 1; // We make sure that the result is between 1 and m.
+      //int j = h[i](S);
+      M[(int)j] = 1; // This casting is ok since j is between 1 and m.
     }
   }
 
