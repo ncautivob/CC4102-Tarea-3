5de2fa05c42005b2eb3b5547b3267f3f74cefbc3
Valentina Alarcón <valentina.alarcon.y@ug.uchile.cl>
2024-07-19 14:32:46 -0400

    Added collision checking

--- bloom.cpp
+++ bloom.cpp
@@ -14,21 +14,14 @@ using namespace std::chrono;
 // HOLI
 // INSTRUCCIONES POR SI LEEN ESTO
 /**
- * Primero: tengo un poco la escoba con los archivos de los resultados
- * No sé por qué no se imprime el primer mensaje xd pero bueno
- * 
- * Tengo comentada una línea del grep search, (alrededor de la 226)
- * Hay que devolverla a como estaba para ver que eso funque
- * Actualmente el error es que se queda pegado y no cacho por qué
- * Pero es en algún lado de la parte del bloom filter XD hay que poner prints
- * 
- * Finalmente, recordar el comando que usamos la otra vez para debuggear
+ * Recordar el comando que usamos la otra vez para debuggear
  * g++ -Wall -O2 -fsanitize=address bloom.cpp MurmurHash3.cpp -o bloom
- *
  * y recuerden usar WSL yay 
 */
 
-
+/** All code associated with murmur hash was done with a bit of investigation
+ * And what could be found on the internet.
+*/
 
 // Applies the murmur hash to the word using a specific seed.
 uint32_t murmur_hash(const string& word, uint32_t seed) {
@@ -37,7 +30,7 @@ uint32_t murmur_hash(const string& word, uint32_t seed) {
     return hash;
 }
 
-// Function that creates hash functions
+// Function that creates hash functions with a certain seed
 function<uint32_t(const string&)> create_murmur_hash_function(uint32_t seed) {
     return bind(murmur_hash, placeholders::_1, seed);
 }
@@ -54,12 +47,14 @@ void deleteDirectory(const string &ruta) {
   filesystem::remove_all(dir);
 }
 
+// Function that checks if a file exists
 bool existsFile(const string &name) {
   ifstream f(name.c_str());
   return f.good();
 }
 
-// Function that writes all names in a file
+// Function that, for grep search,
+// saves both set of search names and multiset of filmnames in the same file
 void names_to_file(unordered_set<string>& search_names,
                   unordered_multiset<string>& search_filmnames,
                   string& filename) {
@@ -77,8 +72,9 @@ void names_to_file(unordered_set<string>& search_names,
   }
 }
 
-// Function that writes all names in a file
-void names_to_file2(unordered_set<string>& search_names,
+// Function that, for bloom filter search,
+// writes all of the names that passed the filter in a file
+void names_to_file2(unordered_multiset<string>& search_names,
                   string& filename) {
   ofstream file(filename);
   if (file.is_open()) {
@@ -91,7 +87,8 @@ void names_to_file2(unordered_set<string>& search_names,
   }
 }
 
-// Function that writes all names in a file
+// Function that writes all names in a file, to apply grep search safely over a txt
+// rather than the csv itself.
 void names_to_file3(vector<string>& names,
                   string& filename) {
   ofstream file(filename);
@@ -107,22 +104,23 @@ void names_to_file3(vector<string>& names,
 
 // Function that applies the grep search!
 void grep_search(string &names_file, string &search_file) {
-  // ifstream infile(s_file);
-  // string term;
-  
-  // while (getline(infile, term)) {
-  //   for (const auto& name: names) {
-  //     if (name==term) {
-  //       cout << "Encontrado: " << name << " con el término: " << term << endl;
-  //     }
-  //   }
-  // }
 
-  // Paso 1: Ejecutar el comando grep
+  /** Explanation of the command:
+   * -F is for 'fixed string' because we want fixed strings rather than to use regular expressions
+   * And we want them to be interpreted just as they are written
+   * Rather than treating special characters as regex things
+   * -x is because we're looking for exact matches
+   * -f is for file! it tells grep to take the search patterns from a file
+   * 
+   * then, 'search_file' is the file that has the strings we will look for in 'names_file'
+   * 
+   * and to correctly retrieve the results afterwards, we redirect them to a txt
+  */
   string command = "grep -F -x -f " + search_file + " " + names_file + " > grep_results.txt";
   system(command.c_str());
 
-  // Paso 2: Leer los resultados de grep
+  // We will now read the results from the txt and print them!
+  // note: this might be deleted soon, as prints add a lot to the execution time...
   ifstream grep_results("grep_results.txt");
   string result;
   while (getline(grep_results, result)) {
@@ -130,16 +128,18 @@ void grep_search(string &names_file, string &search_file) {
   }
   grep_results.close();
 
-  // Limpiar archivo temporal
-  std::remove("grep_results.txt");
+  // We then remove grep results as we don't really need it
+  remove("grep_results.txt");
 }
 
 // Function that applies bloom filter to a name
 int apply_bloom_filter(const string &name, vector<int> &M, vector <function<uint32_t(const string&)>> &h, int k, int m) {
   for (int i = 0; i < k; i++){
-    function<uint32_t(const string&)> hash = h[i];
-    int j = (int) (h[i](name) % m) + 1;
+    function<uint32_t(const string&)> hash = h[i]; // The hash function in the 'i' position
+    int j = (int) (h[i](name) % m) + 1; // We convert the hash result, as it is a uint_32
+    // and we also want it to be between 1 and m!
     if(M[j] != 1){
+      // If even one of the results is not 1, we can say with certainty that the word is NOT in storage.
       return 0;
     }
   }
@@ -157,8 +157,9 @@ int main() {
   vector <function<uint32_t(const string&)>> h(k);
 
   // Create k different hash functions
-  for (int i=0; i<k; ++i) {
-      h[i] = create_murmur_hash_function(i + 1); // Seed i + 1 for uniqueness
+  // We will use the previous 'create murmur hash function' with a different seed every time
+  for (int i = 0; i < k; i++) {
+      h[i] = create_murmur_hash_function(i + 1); // the using of the index i makes it unique!
   }
 
   // Let's import the strings from the two csv files.
@@ -169,6 +170,8 @@ int main() {
   int names_length = names.size();
 
   string names_terms_file = "name_file.txt";
+  // We'll save it in a txt so it's easier to work with later with grep search
+  // This txt will also be called "storage"
   names_to_file3(names, names_terms_file);
 
   rapidcsv::Document doc2("csv/Film-Names.csv");
@@ -178,17 +181,22 @@ int main() {
   int filmnames_length = film_names.size();
 
   // Experimentation:
-  // We should apply the bloom filter to all baby names
+
+  // We should apply the bloom filter to insert all baby names
   // So we have the final 'M' value.
   // With this M value, we can then do the experimentation.
 
   // Insertion using Bloom's filter
   for (string &S: names) {
-    for (int i=0; i<k; i++) {
+    for (int i = 0; i < k; i++) {
       uint32_t j = (h[i](S) % m) + 1; // We make sure that the result is between 1 and m.
       //int j = h[i](S);
       M[(int)j] = 1; // This casting is ok since j is between 1 and m.
     }
+  }
+
+  for (int q = 1; q < m+1 ; q++){
+    cout << "M[" << q << "] is " << M[q] << endl;
   }
 
   vector<int> N_values = {static_cast<int>(pow(2, 10)), static_cast<int>(pow(2, 12)),
@@ -205,7 +213,7 @@ int main() {
   createDirectory("grep-results");
 
   // We'll do this experiment 20 times.
-  // We'll check: search time and error rate.
+  // We'll check: search time and error rate (for bloom filter).
   for(int N : N_values) {
     for(double p : p_values){
       // Unordered set that represents the strings from 'names' that we will search.
@@ -223,19 +231,21 @@ int main() {
         search_names.insert(names[random_number]);
       }
 
-      // Unordered set that represents the strings from 'film names' that we will search.
+      // Unordered multiset that represents the strings from 'film names' that we will search.
+      // It's a multiset because we don't have enough film names to allow all of them to be different
+      // Considering we will need 2^16 different film names, if we took the other approach
       unordered_multiset <string> search_filmnames;
 
       while(search_filmnames.size() < N*(1.0-p)){
-        int random_number = dis_filmnames(gen);
+        int random_number = dis_filmnames(gen); // different distribution due to the different amount
         search_filmnames.insert(film_names[random_number]);
       }
 
       // Now that we have both sets, we should perform the searches!
 
       // First, without Bloom Filter:
-      // We found that, to perform a 'grep' process,
-      // it was necessary to put all 'search names' in a file.
+      // To perform a 'grep' process without any issue,
+      // we put all 'search names' in a file.
       // (it is also important to notice that we don't need the output,
       // just the final time of execution)
 
@@ -278,7 +288,7 @@ int main() {
         outputFile.close();
       }
 
-      // We will now perform the search and check the execution times!
+      // We will now perform the bloom filter search and check the execution times!
       string currentFileName2 =
           "bloom-results/time-results-" + to_string(N) + "-" + to_string(p) + ".txt";
 
@@ -302,13 +312,15 @@ int main() {
 
       // Search each name with the bloom filter
 
-      unordered_set <string> search_after;
+      // originally a set... now a multiset to see what happens!
+      unordered_multiset <string> search_after; // MAYBE THE ISSUE IS THAT THIS IS A SET?
 
       // Let's check for the search names first! They should all pass...
       for (const string &name : search_names) {
           int check = apply_bloom_filter(name, M, h, k, m);
           if(!check){
             // The name did not pass the bloom filter, everything ok.
+            // This never actually happens!
             continue;
           }
           else{
@@ -326,6 +338,7 @@ int main() {
           }
           else{
             // The name passed the bloom filter!
+            cout << "COLLISION!! YAY!!" << endl;
             // We will add it to a list of names we will then search in the actual file.
             search_after.insert(name);
           }
@@ -350,7 +363,9 @@ int main() {
       cout << "Tiempo de ejecución de búsquedas con el filtro de bloom (ms): "
             << to_string(duration2.count()) << '\n'
             << "Porcentaje de error del filtro: "
-            << to_string(error) << '\n';
+            << to_string(error) << '\n'
+            << "Número de colisiones: "
+            << to_string(collisions) << '\n';
 
       cout.rdbuf(originalStdout2);
       outputFile2.close();
